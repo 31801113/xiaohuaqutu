ef6064efce990d57a5fcabb24ac20e818b6833d6
31801113 <48280696+31801113@users.noreply.github.com>
2020-10-18 17:57:58 +0800

    Add files via upload

---
+++ xiaohuaqutu/pages/joke/joke.js
@@ -0,0 +1,79 @@
+// pages/joke/joke.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    jokeList:[
+      {content: '',updatetime:'2018-8-8',url:''},
+      {content: '',updatetime:'2018-8-8',url:''},
+      {content: '',updatetime:'2018-8-8',url:''}
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var thatObj = this;
+    wx.request({
+      url: 'https://v.juhe.cn/joke/content/text.php?key=2ca3e19979fadf2279ac6a036669f5ad&page=2&pagesize=100', //仅为示例，并非真实的接口地址
+      success (res) {
+        //console.log(res.data.result.data)
+        thatObj.setData({
+          jokeList: res.data.result.data
+        });
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

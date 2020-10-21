5a8fc474b98d017abe7b620d2afd1bc5dcc41791
31801113 <48280696+31801113@users.noreply.github.com>
2020-10-21 14:24:50 +0800

    Add files via upload

--- xiaohuaqutu/pages/joke/joke.js
+++ xiaohuaqutu/pages/joke/joke.js
@@ -10,15 +10,17 @@ Page({
       {content: '',updatetime:'2018-8-8',url:''},
       {content: '',updatetime:'2018-8-8',url:''}
     ],
+    page:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.stopPullDownRefresh()
     var thatObj = this;
     wx.request({
-      url: 'https://v.juhe.cn/joke/content/text.php?key=2ca3e19979fadf2279ac6a036669f5ad&page=2&pagesize=100', //仅为示例，并非真实的接口地址
+      url: 'https://v.juhe.cn/joke/content/text.php?key=2ca3e19979fadf2279ac6a036669f5ad&page=' + this.data.page + '&pagesize=20', //仅为示例，并非真实的接口地址
       success (res) {
         //console.log(res.data.result.data)
         thatObj.setData({
@@ -60,7 +62,12 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    var that = this;
+    var a = this.data.page + 1;
+    that.setData({
+      page: a
+    })
+    this.onLoad();
   },
 
   /**
--- xiaohuaqutu/pages/picture/picture.js
+++ xiaohuaqutu/pages/picture/picture.js
@@ -9,16 +9,18 @@ Page({
       {content: '笑话1',updatetime:'2018-8-8',url:''},
       {content: '笑话1',updatetime:'2018-8-8',url:''},
       {content: '笑话1',updatetime:'2018-8-8',url:''}
-    ]
+    ],
+    page:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.stopPullDownRefresh()
     var thatObj = this;
     wx.request({
-      url: 'https://v.juhe.cn/joke/img/list.php?key=2ca3e19979fadf2279ac6a036669f5ad&page=5&pagesize=100&sort=asc&time=1418745237', //仅为示例，并非真实的接口地址
+      url: 'https://v.juhe.cn/joke/img/list.php?key=2ca3e19979fadf2279ac6a036669f5ad&page=' + this.data.page +'&pagesize=20&sort=asc&time=1418745237', //仅为示例，并非真实的接口地址
       success (res) {
       //console.log(res.data.result.data)
         thatObj.setData({
@@ -26,6 +28,15 @@ Page({
         });
       }
     })
+    var that = this; 
+   wx.getSystemInfo({ 
+     success:function(res){ 
+       console.info(res.windowHeight); 
+       that.setData({ 
+         scrollHeight:res.windowHeight 
+       }); 
+     } 
+   }); 
   },
 
   /**
@@ -60,7 +71,13 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    var that = this;
+    var a = this.data.page + 1;
+    that.setData({
+      page: a
+    })
+    this.onLoad();
+    //console.log(this.data.page);
   },
 
   /**
